985e4ae6c5d44a94ccd6d5595b03b241add78d4f
Berlin <50ta2012@gmail.com>
2023-02-21 16:43:09 +0800

    基本 layout 設計

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,30 @@
-import reactLogo from './assets/react.svg'
-import './App.css'
+import reactLogo from "./assets/react.svg";
+import "./App.css";
 
 document.title = "Berlin Website";
 
 function App() {
-
-  return (
-    <div className="App">
-      <div>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Berlin Website</h1>
-      <p>Continue...</p>
-    </div>
-  )
+	return (
+		<div className="App">
+			<div className="layout">
+				<div className="header">
+					<div className="header-grid">
+						<div>
+							<a href="https://reactjs.org" target="_blank">
+								<img src={reactLogo} className="logo react" alt="React logo" />
+							</a>
+						</div>
+						<div>Berlin</div>
+					</div>
+				</div>
+				<div className="body">
+					<h1>Berlin Website</h1>
+					<p>Continue...</p>
+				</div>
+				<div className="footer">footer</div>
+			</div>
+		</div>
+	);
 }
 
-export default App
+export default App;
